5fb6b05dddf3a5ac3ac217d12513d0db6fab3353
thenewlullist <thenewlullist@users.noreply.github.com>
2014-04-24 10:12:24 -0700

    Functions for R Peer Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions assume that the input is always a square invertible matrix
 
-## Write a short comment describing this function
+##makeCacheMatrix() takes a matrix input, calls the solve() function on it and outputs a list which sets/gets the value of the matrix object,
+##then sets/gets the value of its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      mat <- NULL
+      set <- function(y) {
+            x <<- y
+            mat <<- NULL
+      }
+      get <- function() x
+      setsolve <- function(solve) mat <<- solve
+      getsolve <- function() mat
+      list(set = set, get = get,
+           setsolve = setsolve,
+           getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+##cacheSolve() recieves the object output of makeCacheMatrix() above. If the matrix is able to be inverted, and the matrix hasn't changed,
+##then `cacheSolve` retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      mat <- x$getsolve()
+      if(!is.null(mat)) {
+            message("getting cached data")
+            return(mat)
+      }
+      data <- x$get()
+      mat <- solve(data, ...)
+      x$setsolve(mat)
+      mat
 }
